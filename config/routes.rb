6af320e1acf01d742aab0dfda284f3fc5c596127
Mozqu <59715882+Mozqu@users.noreply.github.com>
2020-04-20 21:25:04 +0900

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
   get 'comments/index'
+
   get 're_posts/index'
+
   get 'responses/index'
+
   get 'boards/index'
+
   get 'followers/index'
+
   get 'likes/index'
+
   get 'users/index'
+
   get 'posts/index'
+  get 'posts/new' => 'posts#new'
+  post 'posts/create' => 'posts#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
