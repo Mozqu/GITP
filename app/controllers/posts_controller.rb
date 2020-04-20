fb87a0ec32d2c30e659d4c5c8857fbaf2152b587
Mozqu <59715882+Mozqu@users.noreply.github.com>
2020-04-20 22:44:16 +0900

    a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,14 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(
+
+    )
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_120113) do
+ActiveRecord::Schema.define(version: 2020_04_20_133258) do
 
   create_table "board_likes", force: :cascade do |t|
     t.integer "user_id"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2020_04_18_120113) do
     t.string "image_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
