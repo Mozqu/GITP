b1b06a51add2c04c89125b1939d75e07aa72cdb5
Mozqu <59715882+Mozqu@users.noreply.github.com>
2020-07-13 23:33:07 +0900

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  resources :homes
+   root 'homes#index' 
+
   get 'communities/index' => 'communities#index'
   get 'communities/create_form' => 'communities#new'
   get 'communities/:id' => 'communities#show'
