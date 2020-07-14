00a2239c5de96de06207a3aa9914464dff33bd7e
Mozqu <59715882+Mozqu@users.noreply.github.com>
2020-07-14 17:29:22 +0900

    あ

--- db/migrate/20200624092035_remove_column_from_communities.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnFromCommunities < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :communities, :community_id, :integer
-    remove_column :communities, :user_id, :integer
-    remove_column :communities, :authority, :string
-    remove_column :communities, :allowed, :string
-  end
-end
