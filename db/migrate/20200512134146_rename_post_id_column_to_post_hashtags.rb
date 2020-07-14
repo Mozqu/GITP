51c0a2350fb1be90a384759321a1e8db1643f057
Mozqu <59715882+Mozqu@users.noreply.github.com>
2020-07-14 15:49:51 +0900

    ああ

--- db/migrate/20200512134146_rename_post_id_column_to_post_hashtags.rb
+++
@@ -1,6 +0,0 @@
-class RenamePostIdColumnToPostHashtags < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :post_hashtags, :micropost_id_id, :post_id
-    rename_column :post_hashtags, :hashtag_id_id, :hashtag_id
-  end
-end
