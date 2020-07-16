dd7f9e82ce6cddd3d9327d83a21470ddc7829ca9
Mozqu <59715882+Mozqu@users.noreply.github.com>
2020-07-16 10:20:03 +0900

    board 修正

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,23 +1,21 @@
 $(function() {
 
 
-
+  var id = 0;
   $('#camera').change(function() {
-
-    var file = this.files[0];
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-      //読み込み時に発火するイベント
-      reader.onload = function() {
-        alert('hello');
-        var image = reader.result;
-        $('#camera').before('<img id="image-preview_#{id}" class="image-preview" src="" alt="preview">');
-        $('.image-preview').attr('src', image);
+    if (id < 4) {
+      var file = this.files[0];
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+        //読み込み時に発火するイベント
+        reader.onload = function() {
+          var image = reader.result;
+          $('#camera').before(`<img id="image-preview_${id}"  class="image-preview" src="" alt="preview">`);
+          $(`#image-preview_${id}`).attr('src', image);
+          id += 1;
+        }
+      } else {
+        alert('画像は４枚まで投稿出来ます')
       }
-
-
-  })
-
-
-
+    })
 })
