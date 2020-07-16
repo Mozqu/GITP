a5b14e439cbf0c2d625718401052750a6f4f0087
Mozqu <59715882+Mozqu@users.noreply.github.com>
2020-07-16 16:05:22 +0900

    post投稿ページ
    画像のプレビューと投稿をできるように。まだプレビュー時のキャンセルができない

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -2,8 +2,10 @@ $(function() {
 
 
   var id = 0;
-  $('#camera').change(function() {
+  var target = "#camera0"
+  $(document).on('change', '.hidden-field', function() {
     if (id < 4) {
+      alert("hi");
       var file = this.files[0];
       var reader = new FileReader();
       reader.readAsDataURL(file);
@@ -12,12 +14,18 @@ $(function() {
           var image = reader.result;
           var targetId = "image-preview_" + id;
           var targetIdS = "#" + targetId;
-          $('#camera').before('<img id=' + targetId + ' class="image-preview" src="" alt="preview">');
+          $('#camera').before('<div class="image-preview-container"><img id="' + targetId + '" class="image-preview" src="" alt=""><i id="' + targetId + '-close" class="fa fa-window-close" aria-hidden="true"></i></div>');
           $(targetIdS).attr('src', image);
           id += 1;
+          //labelのforを変える
+          $('#camera').attr('for', 'camera' + id);
+          target = "#camera" + id;
+          alert(target);
         }
       } else {
         alert('画像は４枚まで投稿出来ます')
       }
-    })
+    })//change, hidden-field
+
+
 })
